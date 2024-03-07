6202eb6e97f4a25104a92f671f63cd39ed60fefe
Sebi Mircea <sebimircea53@gmail.com>
2024-03-07 21:08:30 +0200

    new ngrok hotspot test

--- src/app/services/workouts.service.ts
+++ src/app/services/workouts.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class WorkoutsService {
 
-  baseApiURL: string = "https://2ba5-89-137-100-230.ngrok-free.app";
+  baseApiURL: string = "https://3fd6-89-137-100-230.ngrok-free.app";
   constructor(private http: HttpClient) { }
 
   getAllWorkouts(): Observable<Workout[]>{
